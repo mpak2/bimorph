98c73decae42664ca44da34b284c7a381c6c9440
mpak <mpak@mpak.su>
2020-04-21 18:21:18 +0300

    Файлы для разных архитектур

--- f2.sh
+++ f2.sh
@@ -9,4 +9,4 @@
 #riscv64-unknown-elf-gcc -lstdc++ -std=c++14 -lm main.cpp -v -o main && spike pk main #rv-jit main
 
 #riscv64-unknown-elf-gcc -static -v --save-temps -std=c++14 bmf.cpp -o bmf.riscv -lstdc++ -lm && rv-jit bmf #spike pk main
-gcc -static -v -std=c++14 bmf.cpp -o bmf.x86 -lstdc++ -lm && ./bmf.x86
+gcc -static -v -std=c++14 bmf.cpp -o bmf.x86 -lstdc++ -lm && ./bmf.x64
